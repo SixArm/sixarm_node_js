762cc87301f322a0159c4e46b656fcf6f87a9d9d
SixArm <sixarm@sixarm.com>
2011-03-28 16:40:29 -0700

    update node info

--- node_ante.sh
+++ node_ante.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apt-get update
 apt-get install git-core
-apt-get install build-essential openssl libssl-dev
+apt-get install build-essential
+apt-get install openssl libssl-dev libsasl2-2 sasl2-bin libsasl2-modules
--- node_install.sh
+++ node_install.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
-git clone --depth 1 https://github.com/joyent/node.git && cd node
+
+# Step 1: choose where to put node.
+# We choose to put it in /opt 
+
+mkdir -p /opt/node
+cd /opt/node
+
+
+# Step 2: download node.
+#
+# To get node master branch, which may be of alpha/beta quality:
+#   git clone --depth 1 https://github.com/joyent/node.git && cd node
+#
+# To download a particular version of node as a file:
+#   wget http://nodejs.org/dist/node-v0.4.4.tar.gz
+
+wget http://nodejs.org/dist/node-v0.4.4.tar.gz
+tar -zxvf node-v0.4.4.tar.gz
+cd node-v0.4.4.tar.gz
+
+# Step 3. build node.
+
 ./configure
 make
-sudo make install
+make test
+make install
+make doc
